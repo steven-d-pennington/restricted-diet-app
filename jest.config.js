ed13980ea239f230ea7ed64c2d0fcd084255b80a
Steven Pennington <stevepenn@hotmail.com>
2025-08-14 11:53:14 -0700

    test: add smoke test and fix Jest config (mapper, setup) + TS jest types

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)'
   ],
   setupFilesAfterEnv: [
-    '@testing-library/react-native/extend-expect'
+    '@testing-library/jest-native/extend-expect'
   ],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
@@ -20,10 +20,10 @@ module.exports = {
   ],
   testMatch: [
     '**/__tests__/**/*.(ts|tsx)',
-    '**/*.(test|spec).(ts|tsx)'
+    '**/?(*.)+(spec|test).(ts|tsx)'
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   }
 };
---
+++ src/__tests__/smoke.test.ts
@@ -0,0 +1,5 @@
+describe('smoke', () => {
+  it('runs the test environment', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
